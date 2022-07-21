5ada21554cb451d035dcc0eabaf6e14bb868e0dc
Kgiruc <kgiruc76@gmail.com>
2022-07-21 13:23:26 +0200

    navigation scss

--- space/src/components/Links.js
+++ space/src/components/Links.js
@@ -2,18 +2,18 @@ import Logo from "../assets/shared/logo.svg";
 
 function Links() {
     return (
-        <>
+        <header className="menu__container">
         <img src={Logo} alt="logo" />
             <hr></hr>
-        <nav>
+        <nav className="links__container">
             <ul>
-                <li><span>00</span>HOME</li>
-                <li><span>01</span>DESTINATION</li>
-                <li><span>02</span>CREW</li>
-                <li><span>03</span>TECHNOLOGY</li>
+                <li><span>00</span> HOME</li>
+                <li><span>01</span> DESTINATION</li>
+                <li><span>02</span> CREW</li>
+                <li><span>03</span> TECHNOLOGY</li>
             </ul>
         </nav>
-        </>
+        </header>
     );
 }
 
