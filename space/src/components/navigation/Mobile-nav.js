b5eac33677e76fde5b83dd371acf584663b1299c
Kgiruc <kgiruc76@gmail.com>
2022-08-05 13:43:10 +0200

    media mobile hamburger menu

--- space/src/components/navigation/Mobile-nav.js
+++ space/src/components/navigation/Mobile-nav.js
@@ -1,11 +1,28 @@
 import Navlinks from "./Nav-links";
 import hamburger from "../../assets/shared/icon-hamburger.svg";
+import close from "../../assets/shared/icon-close.svg"
+import {useState} from "react";
 
 function MobileNav() {
+
+    const [open, setOpen] = useState(false);
+
+    const hamburgerIcon = <img
+                            src={hamburger}
+                            alt="menu mobile"
+                            onClick={() => setOpen(true)}
+                            />
+
+    const closeIcon = <img
+                            src={close}
+                            alt="menu mobile close"
+                            onClick={() => setOpen(false)}
+                            />
+
     return (
         <nav className="mobile-nav">
-            <img src={hamburger} alt="menu-mobile" />
-        <Navlinks />
+            {open ? closeIcon : hamburgerIcon}
+            {open && <Navlinks />}
         </nav>
     );
 }
